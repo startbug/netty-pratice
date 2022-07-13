c85ba411f9e9fd679af2804cb0125e83ead51b3f
startbug <1285226024@qq.com>
2022-07-13 23:17:12 +0800

    往Channel的TaskQueue中添加任务

--- netty-pro/src/main/java/com/ggs/netty/simple/NettyServerHandler.java
+++ netty-pro/src/main/java/com/ggs/netty/simple/NettyServerHandler.java
@@ -6,6 +6,10 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 说明：
  * 自定义一个Handler需要继承Netty规定的HandlerAdapter(规范)
@@ -25,6 +29,32 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
         // 将msg转换成ByteBuf
         // ByteBuf是Netty提供的，不是NIO的ByteBuffer
         ByteBuf buf = (ByteBuf) msg;
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println(LocalDateTime.now().format(dtf));
+        // 加入此时有一个耗时很长得到业务 --> 异步执行 --> 提交到该Channel的对应的NIOEventLoop的taskQueue
+        ctx.channel().eventLoop().execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                ctx.writeAndFlush(Unpooled.copiedBuffer("hello 客户端-111111111---", CharsetUtil.UTF_8));
+                System.out.println(LocalDateTime.now().format(dtf));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+        ctx.channel().eventLoop().execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                ctx.writeAndFlush(Unpooled.copiedBuffer("hello 客户端-22222222222---", CharsetUtil.UTF_8));
+                System.out.println(LocalDateTime.now().format(dtf));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
 
         System.out.println("客户端发送消息是: " + buf.toString(CharsetUtil.UTF_8));
         System.out.println("客户端地址: " + ctx.channel().remoteAddress());
